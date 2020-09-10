1a430e0e9b98fa5648a8333602d05898c6ce81e4
Abdul Syed <dev.abdul01@gmail.com>
2020-09-10 17:29:24 -0400

    setup query string to be executed to see if user exists

--- sqlite3-python/sql_injection.py
+++ sqlite3-python/sql_injection.py
@@ -1,18 +1,31 @@
 import sqlite3
 conn = sqlite3.connect("sqlite3-python/users.db")
 
-c= conn.cursor()
+
 
 # query = "CREATE TABLE user (username TEXT, password TEXT) "
 # c.execute(query)
 
-list_users = [
-    ("Roald","password123"),
-    ("Rosa", "abc123"),
-	("Henry", "12345")
-]
+# list_users = [
+#     ("Roald","password123"),
+#     ("Rosa", "abc123"),
+# 	("Henry", "12345")
+# ]
 
-c.executemany("INSERT INTO user VALUES (?,?)", list_users)
+# c.executemany("INSERT INTO user VALUES (?,?)", list_users)
+user_name = input("please enter username ")
+password = input("please enter password ")
+query = f"SELECT * FROM user WHERE username='{user_name}' and password='{password}'"
+
+c= conn.cursor()
+
+c.execute(query)
+
+result = c.fetchone()
+if(result):
+    print("welcome back")
+else:
+    print("user does not exist")
 
 
 conn.commit()
