fb2d161a6dfc268b1915b48c2ac1c75c057dbda7
Abdul Syed <dev.abdul01@gmail.com>
2020-09-10 17:22:47 -0400

    adding users and passwd

--- sqlite3-python/sql_injection.py
+++ sqlite3-python/sql_injection.py
@@ -2,8 +2,18 @@ import sqlite3
 conn = sqlite3.connect("sqlite3-python/users.db")
 
 c= conn.cursor()
-query = "CREATE TABLE user (username TEXT, password TEXT) "
 
-c.execute(query)
+# query = "CREATE TABLE user (username TEXT, password TEXT) "
+# c.execute(query)
+
+list_users = [
+    ("Roald","password123"),
+    ("Rosa", "abc123"),
+	("Henry", "12345")
+]
+
+c.executemany("INSERT INTO user VALUES (?,?)", list_users)
+
+
 conn.commit()
 conn.close()
