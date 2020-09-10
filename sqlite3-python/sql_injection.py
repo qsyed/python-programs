d62e6b4776755f6a62012df835a8752a6e7768b6
Abdul Syed <dev.abdul01@gmail.com>
2020-09-10 17:45:04 -0400

    added notes on how sql injection works

--- sqlite3-python/sql_injection.py
+++ sqlite3-python/sql_injection.py
@@ -1,18 +1,32 @@
 import sqlite3
 conn = sqlite3.connect("sqlite3-python/users.db")
 
+"""
+this execrise wa meant to show how sql injection can work if the query strings are not set up properly
+first we have to set up a data base and enter in seed data. 
+
+    the data base was created by using 
+     query = "CREATE TABLE user (username TEXT, password TEXT) "
+    and 
+
+    c.execute(query
+
+    I then created a list of users:
+        list_users = [
+         ("Roald","password123"),
+         ("Rosa", "abc123"),
+ 	     ("Henry", "12345")
+         ]
+
+    the seed data was executed by using:
+    c.executemany("INSERT INTO user VALUES (?,?)", list_users)
+    
 
 
-# query = "CREATE TABLE user (username TEXT, password TEXT) "
-# c.execute(query)
 
-# list_users = [
-#     ("Roald","password123"),
-#     ("Rosa", "abc123"),
-# 	("Henry", "12345")
-# ]
+"""
 
-# c.executemany("INSERT INTO user VALUES (?,?)", list_users)
+
 user_name = input("please enter username ")
 password = input("please enter password ")
 query = f"SELECT * FROM user WHERE username='{user_name}' and password='{password}'"
@@ -30,3 +44,14 @@ else:
 
 conn.commit()
 conn.close()
+
+""" 
+when using f string the query can manipulated using sql:
+enter a name of a user as is and to inject in password field type ' or 1=1--
+
+
+the coorect way to ask a user for input is the following:
+ query = f"SELECT * FROM user WHERE username=? and password=?"
+ followed by c.execute(query,(user_name, password))
+
+"""
