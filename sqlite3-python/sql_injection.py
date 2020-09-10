54c11a714f30b6bef18f9c118e5a034e1f11c33f
Abdul Syed <dev.abdul01@gmail.com>
2020-09-10 17:19:15 -0400

    created db with table

--- sqlite3-python/sql_injection.py
+++ sqlite3-python/sql_injection.py
@@ -2,7 +2,8 @@ import sqlite3
 conn = sqlite3.connect("sqlite3-python/users.db")
 
 c= conn.cursor()
+query = "CREATE TABLE user (username TEXT, password TEXT) "
 
-
+c.execute(query)
 conn.commit()
 conn.close()
