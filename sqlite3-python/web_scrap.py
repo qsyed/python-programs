968bef362c578c0ca24b1cb4ba3876aad741ceed
Abdul Syed <dev.abdul01@gmail.com>
2020-09-10 21:35:43 -0400

    scrapping portion finished; breaking up into different methods

--- sqlite3-python/web_scrap.py
+++ sqlite3-python/web_scrap.py
@@ -8,4 +8,14 @@ response = requests.get("http://books.toscrape.com/catalogue/category/books/hist
 soup = BeautifulSoup(response.text, "html.parser" )
 
 books = soup.find_all("article")
-print(books)
+
+for book in books:
+    title = book.find("h3").find("a")['title']
+    price = book.select(".price_color")[0].get_text()
+    price = float(price.replace("Â","").replace("£",""))
+    paragraph = book.select(".star-rating")[0]
+    ratings = {"Zero": 0, "One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5 }
+    rating = paragraph.get_attribute_list("class")[-1]
+    int_rating = ratings[rating]
+    print(int_rating)
+
