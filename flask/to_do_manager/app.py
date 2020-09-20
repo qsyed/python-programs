d14af73f6df221e2a44005b49d854585823d70f6
Abdul Syed <dev.abdul01@gmail.com>
2020-09-19 21:25:23 -0400

    added rules for db and created db as well

--- flask/to_do_manager/app.py
+++ flask/to_do_manager/app.py
@@ -1,7 +1,26 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+
+# setting up the data base the following lines of code first create a relative path to the db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# the db is initialized using app as settings
+db = SQLAlchemy(app)
+
+
+# setting up the rules for the db, like what pieces are required
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(200), nullable = False)
+    completed = db.Column(db.Integer, default = 0)
+    date_created =  db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
 
 @app.route("/")
 def index():
